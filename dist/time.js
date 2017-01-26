a75b35fcba212131083067d839caea08ee5cef77
Raymond <ray@firestitch.com>
2017-01-26 08:35:35 -0500

    updated time with data() scope and move name to controller

--- dist/time.js
+++ dist/time.js
@@ -1,4 +1,5 @@
 
+
 (function () {
     'use strict';
 
@@ -6,34 +7,36 @@
      * @ngdoc directive
      * @name fs.directives:fs-time
      * @restrict E
-     * @param {object} ng-model The model object
-     * @param {expression} ng-disabled Used to disable the interface
+     * @param {object} fs-model The model object
+     * @param {expression} fs-disabled Used to disable the interface
      * @param {string} fs-label The interface label
      * @param {string} fs-class The class that is inserted into the md-input-container
      * @param {string} fs-hint The interface note
      */
 
     angular.module('fs-angular-time', ['fs-angular-util'])
-    .directive('fsTime', function(fsUtil) {
+    .directive('fsTime', function() {
         return {
             templateUrl: 'views/directives/time.html',
             restrict: 'E',
             scope: {
-                model: '=ngModel',
-                disabled: '=ngDisabled',
+                model: '=fsModel',
+                disabled: '=fsDisabled',
                 label: '@?fsLabel',
                 name: '@?fsName',
                 class: '@?fsClass',
                 required: '=fsRequired',
                 hint: '@?fsHint'
             },
+            controller: ['$scope','fsUtil',function($scope, fsUtil) {
+            	$scope.name = 'input_' + fsUtil.guid();
+            }],
             link: function($scope, element, attr) {
-               $scope.name = $scope.name ? $scope.name : 'input_' + fsUtil.guid();
 
-               var day =  moment().startOf('day');
+            	angular.element(element).data('scope',$scope);
 
+               	var day =  moment().startOf('day');
                 var times = [];
-
                 var length  = 0;
 
                 while (length<1440) {
@@ -41,17 +44,13 @@
                     var label = day.format('h:mm a');
 
                     if(length==0) {
-
                         label = 'midnight';
                     } else if(length==720) {
-
                         label = 'noon';
                     }
 
                     times.push({ value: day.format('HH') + ':' + day.format('mm') + ':00', label: label });
-
                     length += 15;
-
                     day.add(15, 'minutes');
                 }
 
@@ -65,14 +64,22 @@ angular.module('fs-angular-time').run(['$templateCache', function($templateCache
   'use strict';
 
   $templateCache.put('views/directives/time.html',
-    "<md-input-container class=\"{{class}}\">\n" +
-    "\t<label ng-show=\"label\">{{label}}</label>\n" +
-    "\t<md-select ng-model=\"model\" aria-label=\"time\" ng-disabled=\"disabled\" ng-required=\"required\" name=\"{{name}}\">\n" +
-    "\t    <md-option ng-repeat=\"option in times\" value=\"{{::option.value}}\">\n" +
-    "\t        {{::option.label}}\n" +
-    "\t    </md-option>\n" +
-    "\t</md-select>\n" +
-    "        <span ng-show=\"hint\" class=\"hint\">{{hint}}</span>\n" +
+    "<md-input-container class=\"{{class}}\">\r" +
+    "\n" +
+    "\t<label ng-show=\"label\">{{label}}</label>\r" +
+    "\n" +
+    "\t<md-select ng-model=\"model\" aria-label=\"time\" ng-disabled=\"disabled\" ng-required=\"required\" name=\"{{name}}\">\r" +
+    "\n" +
+    "\t    <md-option ng-repeat=\"option in times\" value=\"{{::option.value}}\">\r" +
+    "\n" +
+    "\t        {{::option.label}}\r" +
+    "\n" +
+    "\t    </md-option>\r" +
+    "\n" +
+    "\t</md-select>\r" +
+    "\n" +
+    "        <span ng-show=\"hint\" class=\"hint\">{{hint}}</span>\r" +
+    "\n" +
     "</md-input-container>"
   );
 
--- src/app/scripts/directives/directive.js
+++ src/app/scripts/directives/directive.js
@@ -1,3 +1,4 @@
+
 (function () {
     'use strict';
 
@@ -5,34 +6,36 @@
      * @ngdoc directive
      * @name fs.directives:fs-time
      * @restrict E
-     * @param {object} ng-model The model object
-     * @param {expression} ng-disabled Used to disable the interface
+     * @param {object} fs-model The model object
+     * @param {expression} fs-disabled Used to disable the interface
      * @param {string} fs-label The interface label
      * @param {string} fs-class The class that is inserted into the md-input-container
      * @param {string} fs-hint The interface note
      */
 
     angular.module('fs-angular-time', ['fs-angular-util'])
-    .directive('fsTime', function(fsUtil) {
+    .directive('fsTime', function() {
         return {
             templateUrl: 'views/directives/time.html',
             restrict: 'E',
             scope: {
-                model: '=ngModel',
-                disabled: '=ngDisabled',
+                model: '=fsModel',
+                disabled: '=fsDisabled',
                 label: '@?fsLabel',
                 name: '@?fsName',
                 class: '@?fsClass',
                 required: '=fsRequired',
                 hint: '@?fsHint'
             },
+            controller: ['$scope','fsUtil',function($scope, fsUtil) {
+            	$scope.name = 'input_' + fsUtil.guid();
+            }],
             link: function($scope, element, attr) {
-               $scope.name = $scope.name ? $scope.name : 'input_' + fsUtil.guid();
 
-               var day =  moment().startOf('day');
+            	angular.element(element).data('scope',$scope);
 
+               	var day =  moment().startOf('day');
                 var times = [];
-
                 var length  = 0;
 
                 while (length<1440) {
@@ -40,17 +43,13 @@
                     var label = day.format('h:mm a');
 
                     if(length==0) {
-
                         label = 'midnight';
                     } else if(length==720) {
-
                         label = 'noon';
                     }
 
                     times.push({ value: day.format('HH') + ':' + day.format('mm') + ':00', label: label });
-
                     length += 15;
-
                     day.add(15, 'minutes');
                 }
 

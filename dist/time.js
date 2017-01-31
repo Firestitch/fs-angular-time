555325d8c501109d190498983a123b1c7bbf3a32
Anatoly Thernytskyi <mendor@MacBook-Pro-Anatoly.local>
2017-01-31 14:42:04 +0200

    Optional time intervals

--- dist/time.js
+++ dist/time.js
@@ -12,6 +12,7 @@
      * @param {string} fs-label The interface label
      * @param {string} fs-class The class that is inserted into the md-input-container
      * @param {string} fs-hint The interface note
+     * @param {integer} fs-interval Step intervals. value % 5 should be equal 0
      */
 
     angular.module('fs-angular-time', ['fs-angular-util'])
@@ -26,7 +27,8 @@
                 name: '@?fsName',
                 class: '@?fsClass',
                 required: '@?fsRequired',
-                hint: '@?fsHint'
+                hint: '@?fsHint',
+                interval: '@?fsInterval'
             },
             controller: ['$scope','fsUtil',function($scope, fsUtil) {
             	$scope.name = 'input_' + fsUtil.guid();
@@ -44,6 +46,10 @@
                	var day =  moment().startOf('day');
                 var times = [];
                 var length  = 0;
+                var interval = 15;
+
+                if($scope.interval && $scope.interval % 5 === 0)
+                    interval = parseInt($scope.interval);
 
                 while (length<1440) {
 
@@ -56,8 +62,8 @@
                     }
 
                     times.push({ value: day.format('HH') + ':' + day.format('mm') + ':00', label: label });
-                    length += 15;
-                    day.add(15, 'minutes');
+                    length += interval;
+                    day.add(interval, 'minutes');
                 }
 
                 $scope.times = times;
@@ -70,22 +76,14 @@ angular.module('fs-angular-time').run(['$templateCache', function($templateCache
   'use strict';
 
   $templateCache.put('views/directives/time.html',
-    "<md-input-container class=\"{{class}}\">\r" +
-    "\n" +
-    "\t<label ng-show=\"label\">{{label}}</label>\r" +
-    "\n" +
-    "\t<md-select ng-model=\"model\" aria-label=\"time\" ng-disabled=\"disabled\" name=\"{{name}}\">\r" +
-    "\n" +
-    "\t    <md-option ng-repeat=\"option in times\" value=\"{{::option.value}}\">\r" +
-    "\n" +
-    "\t        {{::option.label}}\r" +
-    "\n" +
-    "\t    </md-option>\r" +
-    "\n" +
-    "\t</md-select>\r" +
-    "\n" +
-    "    <span ng-show=\"hint\" class=\"hint\">{{hint}}</span>\r" +
-    "\n" +
+    "<md-input-container class=\"{{class}}\">\n" +
+    "\t<label ng-show=\"label\">{{label}}</label>\n" +
+    "\t<md-select ng-model=\"model\" aria-label=\"time\" ng-disabled=\"disabled\" name=\"{{name}}\">\n" +
+    "\t    <md-option ng-repeat=\"option in times\" value=\"{{::option.value}}\">\n" +
+    "\t        {{::option.label}}\n" +
+    "\t    </md-option>\n" +
+    "\t</md-select>\n" +
+    "    <span ng-show=\"hint\" class=\"hint\">{{hint}}</span>\n" +
     "</md-input-container>"
   );
 
